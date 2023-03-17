aba97b5f4611cde0a966fc97fe507560e2b1a079
Joanna Kwasniewska <j.kwasniewska88@gmail.com>
2023-03-17 13:05:26 +0100

    Zadanie 0.8 - zmienne w JAVIE

---
+++ kodilla-introkurs/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String topicOfDiscussion = "Mój Pjesek";
+        String nameOfTheDog = "Kumpel";
+        int ageOfTheDog = 7;
+        double weightOfTheDog = 35.5;
+        char collarSize = 'L';
+        boolean isGoodDog = true;
+        System.out.println(topicOfDiscussion);
+        System.out.println(nameOfTheDog);
+        System.out.println(ageOfTheDog);
+        System.out.println(weightOfTheDog);
+        System.out.println(collarSize);
+        System.out.println(isGoodDog);
+    }
+}
