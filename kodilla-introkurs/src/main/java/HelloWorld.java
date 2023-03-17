7b4c0a4c5cc2e6babf24540bbbaf22e52e2768ec
Joanna Kwasniewska <j.kwasniewska88@gmail.com>
2023-03-17 09:53:17 +0100

    FirstCommit

---
+++ kodilla-introkurs/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello from FirstClass!");
+        System.out.println("Asia");
+        // [3]
+        System.out.println(34);
+        System.out.println('K');
+    }
+}
